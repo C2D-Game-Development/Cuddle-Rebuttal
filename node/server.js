50e346c10f07273fd9d815bfa60de9f8b472edb4
Roberto Lopez-Trigo <rlope232@fiu.edu>
2021-02-04 12:40:33 -0500

    yolo2

--- node/server.js
+++ node/server.js
@@ -1,33 +1,30 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const path = require('path');
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const path = require("path");
 
 //state of the game
-io.on('connection', (socket) => {
-    console.log(socket.id, 'a user connected');
-    socket.emit('yourId', { id: socket.id })
 
+io.on("connection", (socket) => {
+  console.log(socket.id, "a user connected");
 
+  socket.emit("yourId", { id: socket.id });
 
+  socket.on("keyFromClient", ({ keyCode, id }) => {
+    console.log(keyCode, id);
+    io.sockets.emit("keyFromServer", { keyCode, id });
+  });
+  socket.on("keyUpFromClient", ({ keyCode, id }) => {
+    console.log(keyCode, id);
+    io.sockets.emit("keyUpFromServer", { keyCode, id });
+  });
 
-
-
-
-
-
-
-    //workspace
-})
-
-
-
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+  //workspace
 });
 
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
 
-
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
