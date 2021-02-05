8bf93b41925d2d6e32415b0f7828def11abe1ffa
Roberto Lopez-Trigo <rlope232@fiu.edu>
2021-02-05 15:00:22 -0500

    sound

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function gameOver() {
     }
   }
 }
-
+let muted = false;
 /////////////////////////////
 //////  Timer ///////////////
 /////////////////////////////
@@ -146,17 +146,39 @@ flowers.src = "./PNG Objects/flower.png";
 let attackDrop = new Image();
 attackDrop.src = "./images/attack-2.png";
 
+///declare Audio variables
+let allAudio = 1;
+let bM = 0.39;
+let kS = 2;
+let SFX = 0.8;
+bM.muted = false;
 /////////////////////////////
 //////Audio for game////////
 /////////////////////////////
 let backgroundMusic = new Audio("./Audio/background-music.mp3");
 backgroundMusic.loop = true;
-backgroundMusic.volume = 0.09;
+backgroundMusic.volume = 0.04 * bM * allAudio;
+
+let playGameAudioX = document.querySelector("#splash-audio");
+playGameAudioX.volume = 0.09 * kS * allAudio;
 
 let killSound = new Audio("./Audio/Kill Sound.mp3");
 killSound.loop = false;
+killSound.volume = 0.24 * kS * allAudio;
 
 let attackSound = new Audio("./Audio/Attack.wav");
+attackSound.volume = 0.09 * SFX * allAudio;
+
+let hit1Sound = new Audio("./Audio/Hit 1.mp3");
+hit1Sound.volume = 0.06 * SFX * allAudio;
+let hit2Sound = new Audio("./Audio/Hit 2.mp3");
+hit2Sound.volume = 0.09 * SFX * allAudio;
+let hit3Sound = new Audio("./Audio/Hit 3.mp3");
+hit3Sound.volume = 0.09 * SFX * allAudio;
+let hit4Sound = new Audio("./Audio/Hit 4.mp3");
+hit4Sound.volume = 0.09 * SFX * allAudio;
+
+////Audio Controls
 
 /////////////////////////////`
 //////Classes for game///////
@@ -354,6 +376,7 @@ class Player {
   receiveDamageP1(multiplier) {
     if (this.blocking == false) {
       this.health -= 10 * multiplier;
+      hit1Sound.play();
       if (player2.direction == "right") {
         this.y -= this.h * 1;
         this.x += this.w;
@@ -363,6 +386,7 @@ class Player {
       }
     } else {
       this.health -= 2 * multiplier;
+      hit2Sound.play();
     }
     bloodP1.sx = 0;
     bloodP1.sy = 0;
@@ -380,6 +404,7 @@ class Player {
   receiveDamageP2(multiplier) {
     if (this.blocking == false) {
       this.health -= 10 * multiplier;
+      hit3Sound.play();
       if (player1.direction == "right") {
         this.y -= this.h * 1;
         this.x -= this.w;
@@ -388,6 +413,7 @@ class Player {
         this.x -= this.w;
       }
     } else {
+      hit4Sound.play();
       this.health -= 2 * multiplier;
     }
 
@@ -900,6 +926,15 @@ function playGame() {
   } else {
     shieldP2.y = -1000;
     player2.blocking = false;
+  }
+  if (keys[77]) {
+    if (bM.muted == false) {
+      bM = 0;
+      bM.muted = true;
+    } else {
+      bM = 0.39;
+      bM.muted = false;
+    }
   }
 
   player2.velY += gravity;
